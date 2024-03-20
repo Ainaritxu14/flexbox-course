7105fa79efe241d1b591d78f9135b7a1f4d32321
Ainaritxu14 <68372956+Ainaritxu14@users.noreply.github.com>
2024-03-20 12:44:06 +0100

    Add files via upload

---
+++ checkpoint7.js
@@ -0,0 +1,24 @@
+function ejercicio() {
+    var num1= 5;
+    var num2= 2;
+    var num3= 8;
+    var num4= 7;
+    var resultado =(num1+num2)*(num3+num4);
+
+if(resultado>50){
+
+    console.log ( "¡El número es mayor que 50!");
+
+    }
+else{
+
+    console.log ( "¡El número es menor que 50!");
+
+    }
+
+return resultado;
+
+    }
+    
+console.log(ejercicio());
+    
